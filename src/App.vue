35070b816ef5967f6f43ad8ee7d582acb9e59a67
nep_char <3368939477@qq.com>
2022-03-16 10:12:04 +0800

    vdm0.1 -> sepi

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col text-center">
+        <h1 class="light">Markdown Preview</h1>
+      </div>
+    </div>
+
+    <div class="row mut-4">
+      <div class="col-md-5">
+        <h4 class="light">Markdown</h4>
+        <textarea
+          class="info"
+          v-model="markdown"
+        />
+      </div>
+      <div class="col-md-5">
+        <h4 class="light">Preview</h4>
+        <div 
+          class="info">
+          {{markdown}}
+        </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      markdown: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+  resize: none;
+  border: 1px dashed orange;
+  outline: none;
+}
+.info {
+  height: 400px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.9);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/style.css'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+createApp(App).mount('#app')
